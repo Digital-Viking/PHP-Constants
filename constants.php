7426786d465f4324f84e3569253b086a9a2599f9
David Weston Webb <westwebb@gmail.com>
2019-01-10 13:14:46 -0500

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$number = 10;
+$number = 100;
+$number = "David";
+echo $number . "<br>";
+
+define("NAME", 1000);
+// anything placed in a "define" is a constant and cannot be changed
+
+
+?>
+
+</body>
+</html>
